6e3e29e0cb71224b06945d7d870b6c027349a786
Robert Sandfield <robert.f.sandfield@gmail.com>
2021-11-30 13:28:52 -0800

    Not found errors

---
+++ models/error.js
@@ -0,0 +1,58 @@
+class HttpError extends Error {
+    constructor(status, name, message) {
+        super(message);
+        this.name = name;
+        this.status = status;
+    }
+
+    toResponse(res) {
+        res.status(this.status).json({ "Error": this.message })
+    }
+}
+
+class NotFoundError extends HttpError {
+    constructor(kind) {
+        super(
+            404,
+            kind + "NotFoundError",
+            kind + " not found"
+        );
+    }
+}
+
+module.exports = {
+    pageNotFound: function (req, res, next) {
+        let error = new this.PageNotFoundError();
+        error.toResponse(res);
+    },
+    MissingAttributeError: class MissingAttributeError extends HttpError {
+        constructor() {
+            super(401, "MissingAttributeError", "Input is missing a required attribute")
+        }
+    },
+    ObjectNotFoundError: class ObjectNotFoundError extends NotFoundError {
+        constructor() {
+            super("Object")
+        }
+    },
+    ChestNotFoundError: class ChestNotFoundError extends NotFoundError {
+        constructor() {
+            super("Chest")
+        }
+    },
+    TreasureNotFoundError: class TreasureNotFoundError extends NotFoundError {
+        constructor() {
+            super("Treasure")
+        }
+    },
+    UserNotFoundError: class UserNotFoundError extends NotFoundError {
+        constructor() {
+            super("User")
+        }
+    },
+    PageNotFoundError: class PageNotFoundError extends NotFoundError {
+        constructor() {
+            super("Page")
+        }
+    },
+}
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
+const error = require("../models/error");
+
 module.exports = function(app) {
     app.use('/chests', './chests');
     app.use('/treasures', './treasures');
     app.use('/users', './users');
+    app.use(error.pageNotFound)
 }
