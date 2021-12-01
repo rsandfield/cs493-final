7f98d654f3bd2f0470ff45a7dc31a566c17e1f92
Robert Sandfield <robert.f.sandfield@gmail.com>
2021-12-01 11:11:54 -0800

    Added auth from hw7

---
+++ services/auth.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+const {google} = require('googleapis');
+
+const oauth2Client = new google.auth.OAuth2(
+    process.env.client_id,
+    process.env.client_secret,
+    process.env.redirect_uri
+);
+
+const scopes = [ 'profile' ];
+
+module.exports = {
+    auth_url: oauth2Client.generateAuthUrl({ scope: scopes }),
+    get_token(req, res, next) {
+        oauth2Client.getToken(req.query.code)
+            .then(tokens => {
+                req.headers.authorization = "Bearer " + tokens["tokens"]["id_token"];
+                next();
+            })
+            .catch(next)
+    },
+    get_user(req, res, next) {
+        if(!req.headers.authorization) return next();
+        let token = req.headers.authorization.substring(7);
+        
+        oauth2Client
+            .verifyIdToken({
+                idToken: token,
+                audience: process.env.client_id
+            })
+            .then(ticket => {
+                req.user = ticket.getPayload();
+                next();
+            })
+            .catch(next);
+    }
+}
