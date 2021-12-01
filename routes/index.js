02506e2b067ca3d5fd82516c02caeefdcb9a405b
Robert Sandfield <robert.f.sandfield@gmail.com>
2021-12-01 12:06:02 -0800

    Added welcome page

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ module.exports = function(app) {
     app.use('/chests', './chests');
     app.use('/treasures', './treasures');
     app.use('/users', './users');
-    app.use(error.pageNotFound)
-    app.use(error.handleErrors)
+    app.use('/', (req, res) => res.render('pages/welcome', { login: '/users/login'}));
+    app.use(error.pageNotFound);
+    app.use(error.handleErrors);
 }
